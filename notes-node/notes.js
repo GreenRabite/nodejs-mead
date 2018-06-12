4e4d0a5218bdfeba7ec21006e405b051460b6a4e
Aluo <green.rabite@gmail.com>
2018-06-11 19:04:45 -0700

    Implemented yargs arguments system

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -4,7 +4,30 @@ console.log("Starting notes.js");
 // You can see that exports is just an object
 
 // module.exports.age = 32;
-module.exports.addNote = ()=>{
-  console.log('addNote');
-  return 'New note';
+// module.exports.addNote = ()=>{
+//   console.log('addNote');
+//   return 'New note';
+// };
+
+const addNote = (title,body) => {
+  console.log("Adding note", title, body);
+};
+
+const getAll = ()=>{
+  console.log('Getting all notes');
+};
+
+const getNote = (title)=>{
+  console.log('Getting note',title);
+};
+
+const removeNote = (title)=>{
+  console.log('Removing note',title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
