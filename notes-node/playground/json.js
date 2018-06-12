74121e5f584e4b571d2244c9243f611933f75478
Aluo <green.rabite@gmail.com>
2018-06-11 20:15:38 -0700

    working with JSON operators

---
+++ notes-node/playground/json.js
@@ -0,0 +1,29 @@
+// let obj = {
+//   name: "Andy"
+// };
+
+// let stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// const personString = '{"name": "Andy", "age":32}';
+// const person = JSON.parse(personString);
+//
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+let originalNote = {
+  title: "Some title",
+  body: "Some body"
+};
+
+// originalNoteString
+const originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+const noteString = fs.readFileSync('notes.json');
+//note
+const note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
